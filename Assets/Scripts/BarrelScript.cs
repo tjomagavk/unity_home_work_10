ee8b0307cba28fa6ea2784f98866119055da1dbd
tjomagavk <tjomagavk@yandex.ru>
2022-01-12 00:15:53 +0300

    second commit

---
+++ Assets/Scripts/BarrelScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public float speed;
+    public float angle;
+    public float vx = 1;
+    public float vy = 1;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayersManager.GetPlayer())
+        {
+            
+            Vector3 direction = Vector3.forward;
+            other.GetComponent<Rigidbody>().AddForce(direction * 200, ForceMode.Impulse);
+            // other.GetComponent<Rigidbody>().AddForce(other.transform.position.normalized * 100, ForceMode.Impulse);
+        }
+    }
+
+   
+}
---
+++ Assets/Scripts/LayersManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class LayersManager : MonoBehaviour
+{
+    private static LayersManager _instance;
+    private int _player;
+    private int _springBarrel;
+
+    public static LayersManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<LayersManager>();
+
+                if (_instance == null)
+                {
+                    GameObject container = new GameObject("LayersManager");
+                    _instance = container.AddComponent<LayersManager>();
+                    _instance.InitializeManager();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private void InitializeManager()
+    {
+        _player = LayerMask.NameToLayer("Player");
+        _springBarrel = LayerMask.NameToLayer("SpringBarrel");
+    }
+
+    public static int GetPlayer()
+    {
+        return Instance._player;
+    }
+
+    public static int GetSpringBarrel()
+    {
+        return Instance._springBarrel;
+    }
+}
