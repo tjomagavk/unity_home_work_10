177c18f3344bb084036907e3577e235143fd6d5b
tjomagavk <tjomagavk@yandex.ru>
2022-01-16 22:09:19 +0300

    add physic material

--- Assets/Scripts/BarrelScript.cs
+++ Assets/Scripts/BarrelScript.cs
@@ -1,36 +1,21 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BarrelScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Light _pointLight;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
-    }
-    
-    public float speed;
-    public float angle;
-    public float vx = 1;
-    public float vy = 1;
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.layer == LayersManager.GetPlayer())
+        if (_pointLight == null)
         {
-            
-            Vector3 direction = Vector3.forward;
-            other.GetComponent<Rigidbody>().AddForce(direction * 200, ForceMode.Impulse);
-            // other.GetComponent<Rigidbody>().AddForce(other.transform.position.normalized * 100, ForceMode.Impulse);
+            _pointLight = GetComponentInChildren<Light>();
         }
+
+        _pointLight.enabled = false;
     }
 
-   
+    private void OnCollisionEnter(Collision other)
+    {
+        _pointLight.enabled = !_pointLight.enabled;
+    }
 }
